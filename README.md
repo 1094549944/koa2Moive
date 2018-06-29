d8dec083105e8b3de5595f6580717227091df1f5
jiaxinying <jiaxinying@91taogu.com>
2018-06-29 17:47:55 +0800

    jia

--- README.md
+++ README.md
@@ -10,9 +10,11 @@ parcel 打包
 
 ## 安装koa 
 npm install koa --save
+
 ## 安装ejs 
 npm install ejs --save
 
 ## 安装jade (pug) 原来叫jade ,现在叫pug
 npm install pug --save
 
+## koa-views  中间件方式，把模板引擎集成到项目里面
--- server/index.js
+++ server/index.js
@@ -1,15 +1,23 @@
 const Koa = require('koa')
 const app = new Koa
 const { normal, ejsTpl, pugTpl } = require('./tpl')
-const ejs = require('ejs')
-const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
 app.use(async (ctx, next) => {
   ctx.type = "text/html;charset=utf-8"
   // ctx.body = ejs.render(ejsTpl, {
   //   you: 'Luck',
   //   me: 'bootstrap'
   // })
-  ctx.body = pug.render(pugTpl, {
+  // ctx.body = pug.render(pugTpl, {
+  //   you: 'Luck',
+  //   me: 'Scott'
+  // })
+
+  await ctx.render('index', {
     you: 'Luck',
     me: 'Scott'
   })
